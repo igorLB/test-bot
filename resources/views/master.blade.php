5108ca8fac78a95c43e86c1bb2f8923b3aec99cd
Igor Cedro <igor.cedro@anexxa.com.br>
2020-07-13 17:37:33 -0300

    change asset() to  secure_asset()

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
         integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
 
 </head>
 <body>
@@ -65,6 +65,6 @@
     </script>
 
 
-    <script src="{{ asset('js/chat.js') }}"></script>
+    <script src="{{ secure_asset('js/chat.js') }}"></script>
 </body>
 </html>
